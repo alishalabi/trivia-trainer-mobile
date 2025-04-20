e336c3698484001968d9e6998e99c011aec9706f
Ali Shalabi <ashalabi13@gmail.com>
2025-04-19 17:16:21 -0700

    Start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect} from "react"
+
+const URL = `https://api.api-ninjas.com/v1/trivia`
+const apiKey = process.env.REACT_APP_SECRET_KEY
 
 function App() {
+
+  const [question, setQuestion] = useState("No question")
+  const [answer, setAnswer] = useState("No answer")
+  const [answerIsVisible, toggleVisible] = useState(false)
+
+  const handleAnswerClick = () => {
+    toggleVisible(true)
+  }
+
+  const fetchData = async () => {
+    toggleVisible(false)
+    const result = await fetch(URL, {
+      headers: {'X-Api-Key': apiKey}
+    })
+    result.json().then(json => {
+      console.log(json)
+      setQuestion(json[0].question)
+      setAnswer(json[0].answer)
+    })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchData}>New Question</button>
+      <h1>Question: {question}</h1>
+      <button onClick={handleAnswerClick}>Show Answer!</button>
+      {answerIsVisible &&
+        <h1>Answer: {answer}</h1>
+      }
     </div>
   );
 }
